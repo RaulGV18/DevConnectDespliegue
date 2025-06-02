df94bdf3266671457b8da0f979015f9e83c36e88
RaulGV18 <rgalval1806@g.educaand.es>
2025-06-02 02:21:51 +0200

    base de datos funcionando y cv tambien

--- backend/src/Controller/CvController.php
+++ backend/src/Controller/CvController.php
@@ -3,48 +3,32 @@
 
 namespace App\Controller;
 
-use Dompdf\Dompdf;
-use Dompdf\Options;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment as TwigEnvironment;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
-class CvController
+class CvController extends AbstractController
 {
     #[Route('/api/generar-cv', name: 'generar_cv', methods: ['POST'])]
-    public function generarCV(Request $request, TwigEnvironment $twig): Response
+    public function generarCv(Request $request): Response
     {
         $data = json_decode($request->getContent(), true);
 
-        // 1. Renderizar plantilla Twig con los datos recibidos
-        $html = $twig->render('cv/plantilla.html.twig', [
-            'nombre' => $data['nombre'] ?? '',
-            'email' => $data['email'] ?? '',
-            'telefono' => $data['telefono'] ?? '',
-            'educacion' => $data['educacion'] ?? [],
-            'experiencia' => $data['experiencia'] ?? [],
-            'habilidades' => $data['habilidades'] ?? [],
+        $html = $this->renderView('cv/template.html.twig', [
+            'data' => $data,
         ]);
 
-        // 2. Generar PDF
-        $options = new Options();
-        $options->set('defaultFont', 'Arial');
-
-        $dompdf = new Dompdf($options);
+        $dompdf = new Dompdf();
         $dompdf->loadHtml($html);
-        $dompdf->setPaper('A4', 'portrait');
         $dompdf->render();
 
-        // 3. Devolver el PDF como respuesta
-        return new Response(
-            $dompdf->output(),
-            200,
-            [
-                'Content-Type' => 'application/pdf',
-                'Content-Disposition' => 'inline; filename="cv.pdf"',
-            ]
-        );
+        return new Response($dompdf->output(), 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'inline; filename="cv.pdf"',
+        ]);
     }
 }
+
